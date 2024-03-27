1f6b7a2fcdb1728c7a9feefcb2646e6207f3de7d
m.cindyhua@gmail.com <cindy.hua@student-cs.fr>
2024-03-27 03:29:26 +0100

    back to before groupid

--- main.py
+++ main.py
@@ -1,53 +1,60 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException, Form
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
 import os
 from pathlib import Path
+from video_sync import create_metadata, simple_json
 import shutil
+from fastapi.responses import FileResponse
 import logging
-from uuid import UUID
-from video_sync import create_metadata
 
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 
 UPLOAD_DIRECTORY = Path("uploaded_audios")
-UPLOAD_DIRECTORY.mkdir(parents=True, exist_ok=True)
+if not UPLOAD_DIRECTORY.exists():
+    UPLOAD_DIRECTORY.mkdir(parents=True)
 print(f"Upload directory is set to {UPLOAD_DIRECTORY.absolute()}")
 
 METADATA_DIRECTORY = Path("metadata")
-METADATA_DIRECTORY.mkdir(parents=True, exist_ok=True)
+if not METADATA_DIRECTORY.exists():
+    METADATA_DIRECTORY.mkdir(parents=True)
 print(f"Metadata directory is set to {METADATA_DIRECTORY.absolute()}")
+received_audios = []
 
-# Stocker les chemins des fichiers audio reçus, classés par UUID de groupe
-received_audios_by_group = {}
+@app.get("/list-audios/")
+def list_audios():
+    files = list(Path("/app/uploaded_audios/").glob('*'))
+    return {"files": [str(file) for file in files]}
 
-@app.post("/upload-audio/{group_id}")
-async def upload_audio(group_id: UUID, audio: UploadFile = File(...)):
+
+@app.post("/upload-audio/")
+async def upload_audio(audio: UploadFile = File(...)):
     content = await audio.read()
     print(f"Received file content size: {len(content)} bytes")
     try:
         print(f"Received file: {audio.filename}")
-        file_location = UPLOAD_DIRECTORY / f"{group_id}_{audio.filename}"
+        print(f"File size: {audio.file._file.tell()} bytes")
+        file_location = UPLOAD_DIRECTORY / audio.filename
         with open(file_location, "wb") as buffer:
+            audio.file.seek(0)  # Remet le pointeur au début du fichier
             shutil.copyfileobj(audio.file, buffer)
         print(f"File saved to {file_location}")
+        
+        received_audios.append(str(file_location))
 
-        # Ajout du fichier au groupe correspondant
-        if group_id not in received_audios_by_group:
-            received_audios_by_group[group_id] = []
-        received_audios_by_group[group_id].append(str(file_location))
-
-        # Vérifier si deux fichiers ont été reçus pour ce groupe
-        if len(received_audios_by_group[group_id]) == 2:
-            # Traitement spécifique pour deux fichiers, par exemple :
-            json_path = create_metadata(received_audios_by_group[group_id], METADATA_DIRECTORY)
-            # Nettoyer après traitement
-            for audio_path in received_audios_by_group[group_id]:
+        if len(received_audios) >= 2:
+            print("About to create metadata")
+            json_path = create_metadata(received_audios, METADATA_DIRECTORY)
+            logging.debug(f"Metadata created at {json_path}")
+            print(f"Metadata created at: {json_path}")
+            print(list_audios())
+            for audio_path in received_audios:
                 os.remove(audio_path)
-            del received_audios_by_group[group_id]
-            # Retourner une réponse appropriée
+            received_audios.clear()
+            
+            # Retourner directement le fichier JSON comme réponse
             return FileResponse(json_path)
         else:
-            return JSONResponse(content={"message": "Fichier reçu, en attente du deuxième."})
+            return JSONResponse(content={"message": "Audio received and waiting for more to process."})
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Could not upload the audio: {e}")
