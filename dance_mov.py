26ecc9423b9cae16590fa647c769f61530410d16
m.cindyhua@gmail.com <cindy.hua@student-cs.fr>
2024-04-01 15:24:41 +0200

    try cv2.createBackgroundSubtractorMOG2()

--- dance_mov.py
+++ dance_mov.py
@@ -1,33 +1,26 @@
 import cv2
-import mediapipe as mp
 
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
+# Initialiser le soustracteur de fond MOG2
+backSub = cv2.createBackgroundSubtractorMOG2()
 
-cap = cv2.VideoCapture('choregraphy/chore1/videos/1.MP4') 
+# Ouvrir la vidéo
+cap = cv2.VideoCapture('choregraphy/chore1/videos/1.MP4')
 
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            continue
+    # Appliquer le soustracteur de fond à la frame
+    fgMask = backSub.apply(frame)
 
-        # Traitement de l'image et détection de pose
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        results = pose.process(image)
+    # Afficher le résultat de la soustraction de fond
+    cv2.imshow('Frame', frame)
+    cv2.imshow('FG Mask', fgMask)
 
-        # Dessin des points de pose sur l'image
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
-        cv2.imshow('MediaPipe Pose', image)
-        if cv2.waitKey(5) & 0xFF == ord('q'):
-            break
+    keyboard = cv2.waitKey(30)
+    if keyboard == 'q' or keyboard == 27:
+        break
 
 cap.release()
+cv2.destroyAllWindows()
